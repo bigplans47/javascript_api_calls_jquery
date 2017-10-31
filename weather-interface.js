15ea2412c9f540657720db526202819a43ec549c
Dan Kiss <flowfast47@gmail.com>
2017-10-31 09:49:06 -0700

    use jquery promise to run code

--- weather-interface.js
+++ weather-interface.js
@@ -3,19 +3,25 @@ $(document).ready(function() {
     let city = $('#location').val();
     console.log(city);
     $('#location').val('');
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text('There was an error processing your request. Please try again.')
-      } //note no comma here after the squiggly bracket as it is the last javascript line of code
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`).then(function(response){
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }).fail(function(error) { //note here compared to line 6
+      $('showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
+    // $.ajax({
+    //   url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`,
+    //   type: 'GET',
+    //   data: {
+    //     format: 'json'
+    //   },
+    //   success: function(response) {
+    //     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    //     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+    //   },
+    //   error: function() {
+    //     $('#errors').text('There was an error processing your request. Please try again.')
+    //   } //note no comma here after the squiggly bracket as it is the last javascript line of code
+    // });
   });
 });
