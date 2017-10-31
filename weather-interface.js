1f0d525a9aaaab18676b18ad9da1b77f032ea79b
Dan Kiss <flowfast47@gmail.com>
2017-10-31 10:14:56 -0700

    adjust code to see error message display

--- weather-interface.js
+++ weather-interface.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     }).fail(function(error) { //note here compared to line 6
-      $('showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+      $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
     // $.ajax({
     //   url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`,
@@ -20,7 +20,7 @@ $(document).ready(function() {
     //     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
     //   },
     //   error: function() {
-    //     $('#errors').text('There was an error processing your request. Please try again.')
+    //     $('.errors').text('There was an error processing your request. Please try again.')
     //   } //note no comma here after the squiggly bracket as it is the last javascript line of code
     // });
   });
